f7ccbe1790d320b8a946768fa1b0db624cb6d021
Andrew Madsen <andrew@openreelsoftware.com>
2019-10-01 11:06:42 -0600

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Andrew R Madsen on 10/1/19.
+//  Copyright © 2019 Open Reel Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

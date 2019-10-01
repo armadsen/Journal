2cfc057e96fd7923facb2a5efe7e8d9fa53bdd7d
Andrew Madsen <andrew@openreelsoftware.com>
2019-10-01 17:30:24 -0600

    Enable CloudKit sync

--- Shared/Sources/Database.swift
+++ Shared/Sources/Database.swift
@@ -34,7 +34,7 @@ class Database {
 		application to it. This property is optional since there are legitimate
 		error conditions that could cause the creation of the store to fail.
 		*/
-		let container = NSPersistentContainer(name: "Journal")
+		let container = NSPersistentCloudKitContainer(name: "Journal")
 		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			if let error = error {
 				// Replace this implementation with code to handle the error appropriately.
